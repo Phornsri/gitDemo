c424b5a907d384671f56320029f990ea2c49d05f
Phornsri <ponsri_Molae074@yahoo.com>
2020-08-01 21:18:21 -0400

    add LogOut.java

--- src/LogOut.java
+++ src/LogOut.java
@@ -1,5 +1,7 @@
 public class LogOut {
     public static void main(String[] args) {
         //get out now
+        //go away
+
     }
 }
--- src/classLogIn.java
+++ src/classLogIn.java
@@ -1,6 +1,6 @@
 public class classLogIn {
     public static void main(String[] args) {
         System.out.println("Please loin!");
-        problem solved!
+        //problem solved!
     }
 }
